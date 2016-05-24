e8c372e2e8d90f86d58c340060ee7bf346dd9870
Vincent Galloy <vgalloy@excilys.com>
2016-05-24 17:08:50 +0200

    [ NEW ] : Team query

---
+++ src/main/java/vgalloy/riot/service/query/impl/team/GetTeamsBySummonerIdsQuery.java
@@ -0,0 +1,50 @@
+package vgalloy.riot.service.query.impl.team;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import vgalloy.riot.api.team.dto.TeamDto;
+import vgalloy.riot.client.RiotWebApi;
+import vgalloy.riot.service.query.AbstractQuery;
+import vgalloy.riot.service.query.DefaultParameter;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 22/05/16.
+ */
+public class GetTeamsBySummonerIdsQuery extends AbstractQuery<Map<String, List<TeamDto>>> {
+
+    private final List<Long> summonerIds = new ArrayList<>();
+
+    /**
+     * Constructor.
+     *
+     * @param riotWebApi       the riot web api for execute query
+     * @param defaultParameter the default query parameter
+     * @param summonerIds      the summoner ids
+     */
+    public GetTeamsBySummonerIdsQuery(RiotWebApi riotWebApi, DefaultParameter defaultParameter, long... summonerIds) {
+        super(riotWebApi, defaultParameter);
+        for (long id : summonerIds) {
+            this.summonerIds.add(id);
+        }
+    }
+
+    /**
+     * Add summoner ids.
+     *
+     * @param summonerIds the summoner ids
+     * @return this
+     */
+    public GetTeamsBySummonerIdsQuery addSummonerIds(long... summonerIds) {
+        for (long id : summonerIds) {
+            this.summonerIds.add(id);
+        }
+        return this;
+    }
+
+    @Override
+    public Map<String, List<TeamDto>> execute() {
+        return riotWebApi.getTeamsBySummonerIds(getRegion(), convert(summonerIds), getRiotApiKeyValue());
+    }
+}
---
+++ src/main/java/vgalloy/riot/service/query/impl/team/GetTeamsByTeamIdsQuery.java
@@ -0,0 +1,50 @@
+package vgalloy.riot.service.query.impl.team;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import vgalloy.riot.api.team.dto.TeamDto;
+import vgalloy.riot.client.RiotWebApi;
+import vgalloy.riot.service.query.AbstractQuery;
+import vgalloy.riot.service.query.DefaultParameter;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 22/05/16.
+ */
+public class GetTeamsByTeamIdsQuery extends AbstractQuery<Map<String, List<TeamDto>>> {
+
+    private final List<Long> teamIds = new ArrayList<>();
+
+    /**
+     * Constructor.
+     *
+     * @param riotWebApi       the riot web api for execute query
+     * @param defaultParameter the default query parameter
+     * @param teamIds          the team ids
+     */
+    public GetTeamsByTeamIdsQuery(RiotWebApi riotWebApi, DefaultParameter defaultParameter, long... teamIds) {
+        super(riotWebApi, defaultParameter);
+        for (long id : teamIds) {
+            this.teamIds.add(id);
+        }
+    }
+
+    /**
+     * Add summoner ids.
+     *
+     * @param teamIds the team ids
+     * @return this
+     */
+    public GetTeamsByTeamIdsQuery addTeamIds(long... teamIds) {
+        for (long id : teamIds) {
+            this.teamIds.add(id);
+        }
+        return this;
+    }
+
+    @Override
+    public Map<String, List<TeamDto>> execute() {
+        return riotWebApi.getTeamsBySummonerIds(getRegion(), convert(teamIds), getRiotApiKeyValue());
+    }
+}
