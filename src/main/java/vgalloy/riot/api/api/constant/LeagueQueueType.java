b1c4b7c7c1c589c1c190f34a4dc0292cbd85cc96
Vincent Galloy <vincent.galloy@eileo.com>
2016-11-24 10:51:54 +0100

    [ CHG ] : Update Dto

--- src/main/java/vgalloy/riot/api/api/constant/LeagueQueueType.java
+++ src/main/java/vgalloy/riot/api/api/constant/LeagueQueueType.java
@@ -5,5 +5,5 @@ package vgalloy.riot.api.api.constant;
  *         Created by Vincent Galloy on 20/05/16.
  */
 public enum LeagueQueueType {
-    RANKED_SOLO_5x5, RANKED_TEAM_3x3, RANKED_TEAM_5x5
+    RANKED_SOLO_5x5, RANKED_TEAM_3x3, RANKED_TEAM_5x5, RANKED_FLEX_SR;
 }
